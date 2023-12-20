e07982f0304bcf540cdb9fd2bd93e321b5c312e1
Root1527 <130300003+Root1527@users.noreply.github.com>
2023-12-20 13:09:48 +0800

    Update booth.lua

--- booth.lua
+++ booth.lua
@@ -97,7 +97,7 @@ local function checklisting(uid, gems, item, version, shiny, amount, username, p
     if string.find(item, "Huge") and gems <= 1000000 then
         game:GetService("ReplicatedStorage").Network.Booths_RequestPurchase:InvokeServer(playerid, uid)
         processListingInfo(uid, gems, item, version, shiny, amount, username)
-    elseif gems <= 1000 then
+    elseif gems <= 10 then
         game:GetService("ReplicatedStorage").Network.Booths_RequestPurchase:InvokeServer(playerid, uid)
         processListingInfo(uid, gems, item, version, shiny, amount, username)
     end
@@ -128,3 +128,36 @@ Booths_Broadcast.OnClientEvent:Connect(function(username, message)
         end
     end
 end)
+
+local function jumpToServer() 
+    local sfUrl = "https://games.roblox.com/v1/games/%s/servers/Public?sortOrder=%s&limit=%s&excludeFullGames=true" 
+    local req = request({ Url = string.format(sfUrl, 15502339080, "Desc", 100) }) 
+    local body = game:GetService("HttpService"):JSONDecode(req.Body) 
+    if config.servers.pageDeep > 1 then 
+        for i = 1, math.random(2, 6), 1 do 
+            req = request({ Url = string.format( sfUrl .. "&cursor=" .. body.nextPageCursor, 15502339080, "Desc", 100 ), }) 
+            body = game:GetService("HttpService"):JSONDecode(req.Body) 
+            task.wait(0.1)
+        end 
+    end 
+    local servers = {} 
+    if body and body.data then 
+        for i, v in next, body.data do 
+            if type(v) == "table" and tonumber(v.playing) and tonumber(v.maxPlayers) and v.playing < v.maxPlayers and v.id ~= game.JobId then
+                table.insert(servers, 1, v.id)
+            end
+        end
+    end
+    local randomCount = #servers
+    if not randomCount then
+        randomCount = 2
+    end
+    game:GetService("TeleportService").TeleportToPlaceInstance(15502339080, servers[math.random(1, randomCount)], game:GetService("Players").LocalPlayer) 
+end
+
+local function chk()
+    wait(300)
+    jumpToServer()
+end
+
+chk()
