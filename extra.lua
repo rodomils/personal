97bb83870c8a16fedd6924f0d5e7e0ad34db3dd1
Root1527 <130300003+Root1527@users.noreply.github.com>
2023-12-21 16:06:21 +0800

    Update extra.lua

--- extra.lua
+++ extra.lua
@@ -3,7 +3,7 @@ repeat wait() until game:IsLoaded()
 local Booths_Broadcast = game:GetService("ReplicatedStorage").Network:WaitForChild("Booths_Broadcast")
 local timer = 0
 local ostime = os.time()
-local message1 = {}
+--local message1 = {}
 
 if not getgenv().a then
     getgenv().a = true
@@ -50,7 +50,7 @@ local function processListingInfo(uid, gems, item, version, shiny, amount, bough
         print("AMOUNT:", amount)
     end
     
-    message1 = {
+    --[[message1 = {
         ['content'] = "Goofyahh Sniper",
         ['embeds'] = {
             {
@@ -87,7 +87,7 @@ local function processListingInfo(uid, gems, item, version, shiny, amount, bough
             ['Content-Type'] = 'application/json';
             };
         Body = jsonMessage;
-    };
+    };]]
 end
 
 local function checklisting(uid, gems, item, version, shiny, amount, username, playerid)
