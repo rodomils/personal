166827c34927e7cbfac17f039b0d482877071b00
Root1527 <130300003+Root1527@users.noreply.github.com>
2023-12-21 15:04:46 +0800

    Update extra.lua

--- extra.lua
+++ extra.lua
@@ -59,22 +59,18 @@ local function processListingInfo(uid, gems, item, version, shiny, amount, bough
                     {
                         ['name'] = "PRICE:",
                         ['value'] = tostring(gems) .. " GEMS",
-                        ['inline'] = true,
                     },
                     {
                         ['name'] = "BOUGHT FROM:",
                         ['value'] = tostring(boughtFrom),
-                        ['inline'] = true,
                     },
                     {
                         ['name'] = "AMOUNT:",
                         ['value'] = tostring(amount),
-                        ['inline'] = true,
                     },
                     {
                         ['name'] = "PETID:",
                         ['value'] = tostring(uid),
-                        ['inline'] = true,
                     },
                 },
             },
