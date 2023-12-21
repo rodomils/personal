e87c564e2ead4ce3ec5c6b536f1a867482a34df2
Root1527 <130300003+Root1527@users.noreply.github.com>
2023-12-21 14:47:11 +0800

    Update extra.lua

--- extra.lua
+++ extra.lua
@@ -83,7 +83,7 @@ local function processListingInfo(uid, gems, item, version, shiny, amount, bough
 
     local http = game:GetService("HttpService")
     local jsonMessage = http:JSONEncode(message)
-
+    print(jsonMessage)
     http:PostAsync(getgenv().Webhook, jsonMessage)
 end
 
