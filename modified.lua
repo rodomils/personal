35d6c59f0b12b1707398d9c554e08ad0b67aafec
Root1527 <130300003+Root1527@users.noreply.github.com>
2023-12-25 20:58:34 +0800

    Update modified.lua

--- modified.lua
+++ modified.lua
@@ -1,5 +1,6 @@
 repeat wait() until game:IsLoaded()
 
+setfpscap(5)
 game:GetService("RunService"):Set3dRenderingEnabled(false)
 local Booths_Broadcast = game:GetService("ReplicatedStorage").Network:WaitForChild("Booths_Broadcast")
 local message1 = {}
