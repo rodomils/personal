2e9559e824e34c860690da0f35cd7d4c08e6f810
Root1527 <130300003+Root1527@users.noreply.github.com>
2023-12-25 13:46:41 +0800

    Update extra.lua

--- extra.lua
+++ extra.lua
@@ -106,7 +106,7 @@ end)
     elseif type.titanic and gems <= 10000000 then
         game:GetService("ReplicatedStorage").Network.Booths_RequestPurchase:InvokeServer(playerid, uid)
         processListingInfo(uid, gems, item, version, shiny, amount, username)      
-    elseif gems <= 1 then
+    elseif gems == 1 then
         game:GetService("ReplicatedStorage").Network.Booths_RequestPurchase:InvokeServer(playerid, uid)
     end
 end
