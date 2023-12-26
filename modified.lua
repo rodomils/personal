725c4cd55ec1fa1fc5beb2dbffab397469ed3fcb
Root1527 <130300003+Root1527@users.noreply.github.com>
2023-12-26 13:41:32 +0800

    Update modified.lua

--- modified.lua
+++ modified.lua
@@ -97,7 +97,8 @@ local function checklisting(uid, gems, item, version, shiny, amount, username, p
 end)
 
     if type.exclusiveLevel and gems <= 10000 and item ~= "Banana" and item ~= "Coin" then
-        game:GetService("ReplicatedStorage").Network.Booths_RequestPurchase:InvokeServer(playerid, uid)
+        local buyPet = game:GetService("ReplicatedStorage").Network.Booths_RequestPurchase:InvokeServer(playerid, uid)
+        print(buyPet)
         processListingInfo(uid, gems, item, version, shiny, amount, username)
     elseif item == "Titanic Christmas Present" and gems <= 25000 then
         game:GetService("ReplicatedStorage").Network.Booths_RequestPurchase:InvokeServer(playerid, uid)
