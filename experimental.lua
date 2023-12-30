81fcaffe932736206a607136a175fb6551a9787d
Root1527 <130300003+Root1527@users.noreply.github.com>
2023-12-30 22:09:20 +0800

    Update experimental.lua

--- experimental.lua
+++ experimental.lua
@@ -1,221 +1,151 @@
-local osclock = os.clock()
-repeat task.wait() until game:IsLoaded()
-
-setfpscap(10)
-game:GetService("RunService"):Set3dRenderingEnabled(false)
-local Booths_Broadcast = game:GetService("ReplicatedStorage").Network:WaitForChild("Booths_Broadcast")
-local Players = game:GetService('Players')
-local getPlayers = Players:GetPlayers()
-local PlayerInServer = #getPlayers
-local http = game:GetService("HttpService")
-local ts = game:GetService("TeleportService")
-
-local vu = game:GetService("VirtualUser")
-Players.LocalPlayer.Idled:connect(function()
-   vu:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
-   task.wait(1)
-   vu:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
-end)
-
-for i = 1, PlayerInServer do
-   if getPlayers[i] ~= Players.LocalPlayer and getPlayers[i].Character then
-      getPlayers[i].Character:ClearAllChildren()
-   end
-   for ii = 1,#alts do
-        if getPlayers[i].Name == alts[ii] and alts[ii] ~= Players.LocalPlayer.Name then
-            jumpToServer()
-        end
-    end
-    if getPlayers[i]:IsInGroup(5060810) or getPlayers[i]:IsInGroup(1200769) then
-        jumpToServer()
-    end
-end
-
-local function processListingInfo(uid, gems, item, version, shiny, amount, boughtFrom, boughtStatus)
-    local gemamount = Players.LocalPlayer.leaderstats["💎 Diamonds"].Value
-    local snipeMessage = Players.LocalPlayer.Name .. " just sniped a "
-    if version then
-        if version == 2 then
-            version = "Rainbow"
-        elseif version == 1 then
-            version = "Golden"
-        end
-    else
-       version = "Normal"
-    end
-    
-    snipeMessage = snipeMessage .. version
-    
-    if shiny then
-        snipeMessage = snipeMessage .. " Shiny"
-    end
-    
-    snipeMessage = snipeMessage .. " " .. (item)
-    
-    if amount == nil then
-        amount = 1
-    end
-
-    if boughtPet == true then
-	local color = tonumber(0x33dd99)
-	local url = webhook
-    else
-	local color = tonumber(0xff00000)
-	local url = webhookFail
-    end
-    
-    local message1 = {
-        ['content'] = "Goofyahh Sniper",
-        ['embeds'] = {
-            {
-                ['title'] = snipeMessage,
-                ["color"] = color,
-                ["timestamp"] = DateTime.now():ToIsoDate(),
-                ['fields'] = {
-                    {
-                        ['name'] = "PRICE:",
-                        ['value'] = tostring(gems) .. " GEMS",
-                    },
-                    {
-                        ['name'] = "BOUGHT FROM:",
-                        ['value'] = tostring(boughtFrom),
-                    },
-                    {
-                        ['name'] = "AMOUNT:",
-                        ['value'] = tostring(amount),
-                    },
-                    {
-                        ['name'] = "REMAINING GEMS:",
-                        ['value'] = tostring(gemamount),
-                    },      
-                    {
-                        ['name'] = "PETID:",
-                        ['value'] = tostring(uid),
-                    },
-                },
-            },
-        }
+wait(15) 
+getgenv().config = {
+        placeId = 8737899170,
+        eventName = "Gingerbread", -- prob to change it to Comet or Coin Jar
+        servers = {
+            count = 100, -- 10, 25, 50, 100
+            sort = "Desc", -- Desc, Asc
+            pageDeep = math.random(2, 6), -- selected players page
+        },
+        delays = {
+            beforeExecute = 0.3,
+            beforeBreak = 1.5,
+            afterBreak = 2.4,
+            hit = 0.03,
+            lootbag = 0.03,
+            beforeTp = 2,
+            whileError = 10,
+        },
     }
 
-    local jsonMessage = http:JSONEncode(message1)
-    local success, response = pcall(function()
-            http:PostAsync(getgenv().webhook, jsonMessage)
-    end)
-    if success == false then
-            local response = request({
-            Url = url,
-            Method = "POST",
-            Headers = {
-                ["Content-Type"] = "application/json"
-            },
-            Body = jsonMessage
-        })
-    end
-end
-
-local function checklisting(uid, gems, item, version, shiny, amount, username, playerid)
-    local Library = require(game.ReplicatedStorage:WaitForChild('Library'))
-    gems = tonumber(gems)
-    local type = {}
-    pcall(function()
-        type = Library.Directory.Pets[item]
-    end)
-
-    if type.exclusiveLevel and gems <= 10000 and item ~= "Banana" and item ~= "Coin" then
-        local boughtPet, boughtMessage = game:GetService("ReplicatedStorage").Network.Booths_RequestPurchase:InvokeServer(playerid, uid)
-        if boughtPet == true then
-            processListingInfo(uid, gems, item, version, shiny, amount, username, boughtPet)
-        end
-    elseif item == "Titanic Christmas Present" and gems <= 25000 then
-        local boughtPet, boughtMessage = game:GetService("ReplicatedStorage").Network.Booths_RequestPurchase:InvokeServer(playerid, uid)
-        if boughtPet == true then
-            processListingInfo(uid, gems, item, version, shiny, amount, username, boughtPet)
-        end
-    elseif string.find(item, "Exclusive") and gems <= 25000 then
-        local boughtPet, boughtMessage = game:GetService("ReplicatedStorage").Network.Booths_RequestPurchase:InvokeServer(playerid, uid)
-        if boughtPet == true then
-            processListingInfo(uid, gems, item, version, shiny, amount, username, boughtPet)
-        end
-    elseif type.huge and gems <= 1000000 then
-        local boughtPet, boughtMessage = game:GetService("ReplicatedStorage").Network.Booths_RequestPurchase:InvokeServer(playerid, uid)
-        if boughtPet == true then
-            processListingInfo(uid, gems, item, version, shiny, amount, username, boughtPet)
-        end     
-    elseif type.titanic and gems <= 10000000 then
-        local boughtPet, boughtMessage = game:GetService("ReplicatedStorage").Network.Booths_RequestPurchase:InvokeServer(playerid, uid)
-        if boughtPet == true then
-            processListingInfo(uid, gems, item, version, shiny, amount, username, boughtPet)
-        end
-    end
-end
-
-Booths_Broadcast.OnClientEvent:Connect(function(username, message)
-    pcall(function() local playerID = message['PlayerID'] end)
-    if type(message) == "table" then
-        local listing = message["Listings"]
-        for key, value in pairs(listing) do
-            if type(value) == "table" then
-                local uid = key
-                local gems = value["DiamondCost"]
-                local itemdata = value["ItemData"]
-
-                if itemdata then
-                    local data = itemdata["data"]
-
-                    if data then
-                        local item = data["id"]
-                        local version = data["pt"]
-                        local shiny = data["sh"]
-                        local amount = data["_am"]
-                        checklisting(uid, gems, item, version, shiny, amount, username , playerID)
-                    end
-                end
-            end
-        end
-    end
-end)
-
-local function jumpToServer() 
+if not getgenv().config then 
+    getgenv().config = { 
+        placeId = 8737899170, 
+        eventName = "Gingerbread",
+        count = 100, 
+        sort = "Asc", 
+        pageDeep = math.random(2, 10), 
+        delays = { beforeExecute = 0.3, beforeBreak = 1.5, afterBreak = 2.4, hit = 0.03, lootbag = 0.03, beforeTp = 2, whileError = 10, }, 
+    } 
+end 
+repeat wait() until game.PlaceId ~= nil 
+if not game:IsLoaded() then
+        game.Loaded:Wait() 
+end 
+local ReplicatedStorage = game:GetService("ReplicatedStorage") 
+local HttpService = game:GetService("HttpService") 
+local Players = game:GetService("Players") 
+local TeleportService = game:GetService("TeleportService") 
+wait(config.delays.beforeExecute) 
+if game.PlaceId ~= config.placeId then print("Gingerbread hunter unloaded, unknown place.")
+        return
+end 
+local Library = require(ReplicatedStorage:WaitForChild("Library", 2000)) 
+if not Library.Loaded then
+        repeat wait() until Library.Loaded ~= false
+end 
+local RandomEventCmds = Library.RandomEventCmds 
+local LocalPlayer = Players.LocalPlayer 
+local Character = LocalPlayer.Character 
+local Humanoid = Character:WaitForChild("Humanoid", 1000) 
+local HumanoidRootPart = Character:WaitForChild("HumanoidRootPart", 1000) 
+print(HttpService:JSONEncode(config)) 
+function tpToPos(cframe) 
+    HumanoidRootPart.CFrame = CFrame.new(cframe) 
+end 
+function jumpToServer() 
     local sfUrl = "https://games.roblox.com/v1/games/%s/servers/Public?sortOrder=%s&limit=%s&excludeFullGames=true" 
-    local req = request({ Url = string.format(sfUrl, 15502339080, "Desc", 100) }) 
-    local body = http:JSONDecode(req.Body) 
-    local deep = math.random(1, 3)
-    if deep > 1 then 
-        for i = 1, deep do 
-             req = request({ Url = string.format(sfUrl .. "&cursor=" .. body.nextPageCursor, 15502339080, "Desc", 100) }) 
-             body = http:JSONDecode(req.Body) 
-             task.wait(0.1)
+    local req = request({ Url = string.format(sfUrl, config.placeId, config.servers.sort, config.servers.count) }) 
+    local body = HttpService:JSONDecode(req.Body) 
+    if config.servers.pageDeep > 1 then 
+        for i = 1, config.servers.pageDeep, 1 do 
+            req = request({ Url = string.format( sfUrl .. "&cursor=" .. body.nextPageCursor, config.placeId, config.servers.sort, config.servers.count ), }) 
+            body = HttpService:JSONDecode(req.Body) 
+            wait(0.1) 
         end 
     end 
     local servers = {} 
     if body and body.data then 
-        for i = 1, #body.data do 
-            if type(i) == "table" and tonumber(i.playing) and tonumber(i.maxPlayers) and i.playing < i.maxPlayers and i.id ~= game.JobId then
-                table.insert(servers, 1, i.id)
-            end
-        end
-    end
-    local randomCount = #servers
+        for i, v in next, body.data do 
+            if type(v) == "table" and tonumber(v.playing) and tonumber(v.maxPlayers) and v.playing < v.maxPlayers and v.id ~= game.JobId then
+                table.insert(servers, 1, v.id) 
+            end 
+        end 
+    end 
+    local randomCount = #servers 
     if not randomCount then
        randomCount = 2
-    end
-    ts:TeleportToPlaceInstance(15502339080, servers[math.random(1, randomCount)], game:GetService("Players").LocalPlayer) 
-end
-
-Players.PlayerAdded:Connect(function(player)
-    if player:IsInGroup(5060810) or player:IsInGroup(1200769) then
-        jumpToServer()
-    end
-    for i = 1,#alts do
-        if  player.Name == alts[i] and alts[i] ~= Players.LocalPlayer.Name then
-            jumpToServer()
+    end 
+    TeleportService:TeleportToPlaceInstance(config.placeId, servers[math.random(1, randomCount)], Players.LocalPlayer) 
+end 
+Library.Alert.Message("Finding Gingerbread...") 
+local activeEvents = RandomEventCmds.GetActive() or RandomEventCmds.GetActive()
+local isGingerbreadExist = false
+wait(config.delays.beforeExecute)
+for eventId, event in activeEvents do
+        if event.name == config.eventName then
+                isGingerbreadExist = true
+                tpToPos(event.origin + Vector3.new(0, 18, 0))
         end
-    end
+end 
+Library.Things:FindFirstChild("Lootbags").ChildAdded:Connect(function(lootbag)
+                wait()
+                if lootbag then
+                        game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Lootbags_Claim"):FireServer(unpack({ [1] = { [1] = lootbag.Name, }, }))
+                end 
+end)  
+function CollectAllLootbags() pcall(function() 
+        for _, lootbag in pairs(Library.Things:FindFirstChild("Lootbags"):GetChildren()) do
+                if lootbag and not lootbag:GetAttribute("Collected") then
+                        game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Lootbags_Claim"):FireServer(unpack({ [1] = { [1] = lootbag.Name, }, }))
+                                wait(config.delays.lootbag)
+                end
+        end
 end) 
-
-game:GetService("RunService").Stepped:Connect(function()
-    PlayerInServer = #getPlayers
-    if PlayerInServer < 25 or math.floor(os.clock() - osclock) >= math.random(900, 1200) then
-        jumpToServer()
-    end
-end)
+end 
+function findGingerbread() 
+        for index, breakable in Library.Things.Breakables:GetChildren() do
+                if breakable.ClassName == "Model" and breakable:GetAttribute("BreakableID") == config.eventName then
+                        return breakable
+                end
+        end
+end 
+if isGingerbreadExist then
+        Library.Alert.Message("Gingerbread exist!")
+        wait(config.delays.beforeBreak)
+        local findedGingerbread = nil
+        for i = 1, 5, 1 do
+                findedGingerbread = findGingerbread()
+                if findedGingerbread then
+                        tpToPos(findedGingerbread.PrimaryPart.Position + Vector3.new(0, 18, 0))
+                        break
+                else
+                        wait(0.5)
+                end
+        end
+        if findedGingerbread then
+                Library.Alert.Message("Start breaking!")
+                while Library.Things.Breakables:FindFirstChild(findedGingerbread.Name) do
+                        local args = { [1] = findedGingerbread.Name, } game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Breakables_PlayerDealDamage"):FireServer(unpack(args))
+                        wait(config.delays.hit)
+                end
+                Library.Alert.Message("Broke!")
+                findedGingerbread = false
+        end
+        CollectAllLootbags()
+        wait(config.delays.afterBreak)
+        CollectAllLootbags()
+        wait(config.delays.afterBreak)
+else
+        Library.Alert.Message("Gingerbread not found :c")
+end
+TeleportService.TeleportInitFailed:Connect(function(player, resultEnum, msg)
+                print(string.format("server: teleport %s failed, resultEnum:%s, msg:%s", player.Name, tostring(resultEnum), msg))
+                config.servers.pageDeep = config.servers.pageDeep + 1
+                Library.Alert.Message("Tp Retry... :" .. msg)
+                wait(config.delays.whileError)
+                jumpToServer()
+        end) 
+wait(config.delays.beforeTp)
+Library.Alert.Message("Tp to another server...")
+jumpToServer()
