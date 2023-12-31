eb7e6beb4cb5277bd2a9f05453ad2aa9b389f1e9
Root1527 <130300003+Root1527@users.noreply.github.com>
2023-12-31 14:04:12 +0800

    Update experimental.lua

--- experimental.lua
+++ experimental.lua
@@ -20,33 +20,6 @@ game:GetService("StarterGui"):SetCore("SendNotification",{
 		Text = 'Type "/console" in chat to know what this is for',
 		Duration = 10,
 	})
-	
-print("Basically there's a unique achievement which you can get in Pet Simulator 99 called 'Is It Real?' which gives you a huge pet which at the moment only has 2 player that has it. Everytime you ascend into the skies, there is a 1 in 1 Million chance that you will get the pet. It is unique and can be traded for a bunch of titanics. This script automates the whole climbing thing as fast as possible, good luck!")
-
-local coordinates
-local notifs = loadstring(game:HttpGet('https://raw.githubusercontent.com/CF-Trail/random/main/FE2Notifs.lua'))()
-
-notifs.alert('Execute "_G.s = false" to stop!\nThis is the fastest it can go due to stairs being generated.', nil, 1000000, 'rainbow')
-task.wait(0.01)
-
-local lastNotificationTime = 0
-local notificationDelay = 0.5
-
-local function updateCoordinates()
-    local player = game.Players.LocalPlayer
-    local character = player.Character or player.CharacterAdded:Wait()
-    local humanoidRootPart = character:WaitForChild("HumanoidRootPart")
-
-    coordinates = humanoidRootPart.Position.Y -- Only the Y axis
-
-    local currentTime = tick()
-    if currentTime - lastNotificationTime >= notificationDelay then
-        notifs.alert('Studs above the sky: ' .. tostring(math.floor(coordinates)) .. '', nil, 0.5) -- Display Y axis without decimals
-        lastNotificationTime = currentTime
-    end
-end
-
-game:GetService("RunService").Heartbeat:Connect(updateCoordinates)
 
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(120.79306030273438, -126.99183654785156, -213.44664001464844)
 
@@ -57,11 +30,12 @@ game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(630.6519
 task.wait(1)
 
 local s = false
+local Players = game:GetService("Players")
 local function updateYCoordinate()
-    local currentCFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
+    local currentCFrame = Players.LocalPlayer.Character.HumanoidRootPart.CFrame
     local currentPosition = currentCFrame.Position
     currentPosition = Vector3.new(currentPosition.X, currentPosition.Y + 36, currentPosition.Z)
-    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(currentPosition)
+    Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(currentPosition)
 end
 
 local connect
@@ -87,13 +61,13 @@ connect = game:GetService("RunService").Heartbeat:Connect(function()
     	if success == false then
             local response = request({
                 Url = weburl,
-           	 Method = "POST",
-            Headers = {
-                ["Content-Type"] = "application/json"
-            },
-            Body = jsonMessage
+           	Method = "POST",
+            	Headers = {
+                	["Content-Type"] = "application/json"
+            	},
+           	Body = jsonMessage
         })
-    end
+    	end
 	connect:Disconnect()
     end
 end)
