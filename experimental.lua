f3582d69a0147838215273d9f75b99718cd77109
Root1527 <130300003+Root1527@users.noreply.github.com>
2023-12-31 14:11:27 +0800

    Update experimental.lua

--- experimental.lua
+++ experimental.lua
@@ -1,43 +1,3 @@
--- I would appreciate if the credits doesn't get removed, ty!
--- Credits: "fissurectomy" in Discord without the quotes!
--- Modified by root (s.g.i)
-
-local vu = game:GetService("VirtualUser")
-game:GetService("Players").LocalPlayer.Idled:connect(function()
-   vu:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
-   task.wait(1)
-   vu:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
-end)
-
-if Stairway == true then
-	return
-end
-
-pcall(function() getgenv().Stairway = true end)
-
-game:GetService("StarterGui"):SetCore("SendNotification",{
-		Title = "More Info",
-		Text = 'Type "/console" in chat to know what this is for',
-		Duration = 10,
-	})
-
-game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(120.79306030273438, -126.99183654785156, -213.44664001464844)
-
-task.wait(5)
-
-game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(630.6519165039062, 143.7024383544922, -1891.4598388671875)
-
-task.wait(1)
-
-local s = false
-local Players = game:GetService("Players")
-local function updateYCoordinate()
-    local currentCFrame = Players.LocalPlayer.Character.HumanoidRootPart.CFrame
-    local currentPosition = currentCFrame.Position
-    currentPosition = Vector3.new(currentPosition.X, currentPosition.Y + 36, currentPosition.Z)
-    Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(currentPosition)
-end
-
 local connect
 
 connect = game:GetService("RunService").Heartbeat:Connect(function()
@@ -48,7 +8,7 @@ connect = game:GetService("RunService").Heartbeat:Connect(function()
 		Text = 'wtf how',
 		Duration = 10,
 	})
-	s = true
+	s = false
 
 	local message1 = {
             ['content'] = "@everyone yo congrats on the first huge golden angel dog in a long time",
@@ -71,8 +31,3 @@ connect = game:GetService("RunService").Heartbeat:Connect(function()
 	connect:Disconnect()
     end
 end)
-
-while s == false do
-    updateYCoordinate()
-    wait(0.16)
-end
