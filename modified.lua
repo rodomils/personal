e6f90c6771759f1c992cdca728eb9e29214677b1
Root1527 <130300003+Root1527@users.noreply.github.com>
2024-01-05 09:17:06 +0800

    Update modified.lua

--- modified.lua
+++ modified.lua
@@ -126,7 +126,7 @@ end
 
 local function tryPurchase(uid, gems, item, version, shiny, amount, username, class, playerid, buytimestamp, listTimestamp, snipeNormal)
     if buytimestamp > listTimestamp then
-      task.wait(3.4 - (Players.LocalPlayer:GetNetworkPing() * 2))
+      task.wait(3.4 - Players.LocalPlayer:GetNetworkPing())
     end
     local boughtPet, boughtMessage = game:GetService("ReplicatedStorage").Network.Booths_RequestPurchase:InvokeServer(playerid, uid)
     processListingInfo(uid, gems, item, version, shiny, amount, username, boughtPet, class, boughtMessage, snipeNormal)
