20122b6b58ea8a52ed58ed12f30dbd53846600f9
Root1527 <130300003+Root1527@users.noreply.github.com>
2024-01-09 20:07:54 +0800

    Update modified.lua

--- modified.lua
+++ modified.lua
@@ -139,9 +139,9 @@ local function processListingInfo(uid, gems, item, version, shiny, amount, bough
 end
 
 local function tryPurchase(uid, gems, item, version, shiny, amount, username, class, playerid, buytimestamp, listTimestamp, snipeNormal)
-    print(tick())
+    print(tick() - 28800)
     print(buytimestamp - Players.LocalPlayer:GetNetworkPing())
-    repeat task.wait() until tick() > buytimestamp - Players.LocalPlayer:GetNetworkPing()
+    repeat task.wait() until tick() - 28800 > buytimestamp - Players.LocalPlayer:GetNetworkPing()
     local boughtPet, boughtMessage = game:GetService("ReplicatedStorage").Network.Booths_RequestPurchase:InvokeServer(playerid, uid)
     processListingInfo(uid, gems, item, version, shiny, amount, username, boughtPet, class, boughtMessage, snipeNormal)
 end
