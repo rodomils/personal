3ef3004c1b8443b51a80bf9c8af8fbe557cd99a3
Root1527 <130300003+Root1527@users.noreply.github.com>
2024-01-09 16:46:40 +0800

    Update modified.lua

--- modified.lua
+++ modified.lua
@@ -136,6 +136,7 @@ local function tryPurchase(uid, gems, item, version, shiny, amount, username, cl
         boughtPet, boughtMessage = game:GetService("ReplicatedStorage").Network.Booths_RequestPurchase:InvokeServer(playerid, uid)
         task.wait()
     until buytimestamp <= os.clock()
+    task.wait(3)
     processListingInfo(uid, gems, item, version, shiny, amount, username, boughtPet, class, boughtMessage, snipeNormal)
 end
 
